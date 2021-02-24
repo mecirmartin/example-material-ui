8de07a4d832d200255cbe8dd4a5dddbc107b0c7e
mecirmartin <mecir.martin@gmail.com>
2021-02-24 11:29:12 +0100

    added console.logs() to debug behaviour in iframe

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import 'react-perfect-scrollbar/dist/css/styles.css';
 import React from 'react';
-import { useRoutes } from 'react-router-dom';
+import { useRoutes, useLocation } from 'react-router-dom';
+
 import { ThemeProvider } from '@material-ui/core';
 import GlobalStyles from 'src/components/GlobalStyles';
 import 'src/mixins/chartjs';
@@ -9,7 +10,8 @@ import routes from 'src/routes';
 
 const App = () => {
   const routing = useRoutes(routes);
-
+  const history = useLocation();
+  console.log('toto', history);
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
