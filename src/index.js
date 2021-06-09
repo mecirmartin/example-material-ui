31df55017d825ee0a07ac8b3ed3ebd06446815c7
mecirmartin <55835379+mecirmartin@users.noreply.github.com>
2021-06-09 07:21:19 +0200

    added semicolon

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ import * as Yup from 'yup';
 import * as ReactIntl from 'react-intl';
 import * as MaterialUiDataGrid from '@material-ui/data-grid';
 import gql from 'graphql-tag';
-import * as Urql from 'urql'
+import * as Urql from 'urql';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
